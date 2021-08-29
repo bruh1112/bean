6e95c09652b020c297af4fecb6c152d6f90a0321
joshbc2304 <62204134+joshbc2304@users.noreply.github.com>
2021-08-29 15:13:09 +0800

    Update task7.py
    sdfasdfgsdfhjsdfghjgsghjasdf

--- task7.py
+++ task7.py
@@ -10,7 +10,7 @@ def buy():
     if s1.get().isnumeric() and s2.get().isnumeric() and s3.get().isnumeric() and s4.get().isnumeric():
         print(str(int(s1.get()) * cap))
     else:
-        print("gh")drfghhdughdjkghdkfjsdfghjk
+        print("gh")
 
 defa, defa1, defa2, defa3 = tk.IntVar(0), tk.IntVar(0), tk.IntVar(0), tk.IntVar(0)
 check = tk.IntVar()
@@ -32,4 +32,4 @@ l1.pack(), l2.pack(), l3.pack(), l4.pack()
 l1.place(x=20, y=50), l2.place(x=20, y=70), l3.place(x=20, y=90), l4.place(x=20, y=110)
 s1.pack(), s2.pack(), s3.pack(), s4.pack()
 s1.place(x=100, y=50), s2.place(x=100, y=70), s3.place(x=100, y=90), s4.place(x=100, y=110)
-root.mainloop()
+root.mainloop() #HELLO
